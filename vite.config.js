315f5d349fd88a9147c0186ff893e075ea63a21d
Kri <kri.boats@gmail.com>
2025-07-10 18:14:22 +0200

    deployment config stuff

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kri-boats/',
+  base: '/',
   plugins: [react({
     jsxRuntime: 'automatic'
   })],
